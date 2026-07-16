25b93f4e5bcd71cffc817dcf715f1a402c09b434
Goldmetal <gold_metal@naver.com>
2026-07-16 20:31:11 +0900

    Add demo appkey

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -3,6 +3,14 @@ using UnityEngine.Events;
 
 public class AdManager : MonoBehaviour
 {
+	// Android
+	// appKey= 25b63cf85
+	// adUnityId= syz3d8ekts22q0or
+	
+	// iOS
+	// appKey= 25c43a4a5
+	// adUnityId= l1quzz1xmmdhw5er
+	
     [Space(3)]
     [SerializeField]
     UnityEvent onRewarded;
